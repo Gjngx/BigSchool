3af8ce63deed4a50b0c4ea8e8147b9eb7d8a8f6d
Gjngx <giangdang2k2@gmail.com>
2023-03-16 01:21:10 +0700

    BigSchool Trang 35

--- DangNhatGiang_2011060234/Controllers/AccountController.cs
+++ DangNhatGiang_2011060234/Controllers/AccountController.cs
@@ -151,7 +151,7 @@ namespace DangNhatGiang_2011060234.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, Name = model.Name };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
--- DangNhatGiang_2011060234/Models/ViewModels/CourseViewModel.cs
+++ DangNhatGiang_2011060234/Models/ViewModels/CourseViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,14 +8,20 @@ namespace DangNhatGiang_2011060234.Models.ViewModels
 {
     public class CourseViewModel
     {
+        [Required]
         public string Place { get; set; }
+        [Required]
+        [FutureDate]
         public string Date { get; set; }
+        [Required]
+        [ValidTime]
         public string Time { get; set; }
+        [Required]
         public byte Category { get; set; }
         public IEnumerable<Category> Categories { get; set; }  
         public DateTime GetDateTime()
         {
-            return DateTime.Parse(string.Format("{0}{1}", Date, Time));
+            return DateTime.Parse(string.Format("{0} {1}", Date,Time));
         }
 
     }
