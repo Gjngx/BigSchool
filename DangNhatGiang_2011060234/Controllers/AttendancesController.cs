d30653ac86e276513739838ceb802973f4290a71
Gjngx <giangdang2k2@gmail.com>
2023-03-21 10:36:26 +0700

    Trang 45

---
+++ DangNhatGiang_2011060234/Controllers/AttendancesController.cs
@@ -0,0 +1,36 @@
+﻿using DangNhatGiang_2011060234.DTOs;
+using DangNhatGiang_2011060234.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DangNhatGiang_2011060234.Controllers
+{
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _dbcontext;
+        public AttendancesController()
+        {
+            _dbcontext = new ApplicationDbContext();
+        }
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto attendanceDto)
+        {
+            var userId = User.Identity.GetUserId();
+            if (_dbcontext.Attendances.Any(a => a.AttendeeId == userId && a.CourseId == attendanceDto.CourseId))
+                return BadRequest("The Attendance already exists!");
+            var attendance = new Attendance
+            {
+                CourseId = attendanceDto.CourseId,
+                AttendeeId = userId
+            };
+            _dbcontext.Attendances.Add(attendance);
+            _dbcontext.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ DangNhatGiang_2011060234/DTOs/AttendanceDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DangNhatGiang_2011060234.DTOs
+{
+    public class AttendanceDto
+    {
+        public int CourseId { get; set; }
+    }
+}
